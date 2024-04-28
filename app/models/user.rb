33cc75d0b64f88acac0ff3f97701386b8f019c8d
Pedro Lages <pedro.slages3@gmail.com>
2024-04-28 00:42:14 -0300

    feat(followers): assert new created users always follow themselves

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
   has_many :posts
 
-  # persons that user is following
   has_many :following_relationships, foreign_key: :follower_id, class_name: 'Following'
   has_many :following, through: :following_relationships, source: :followed
 
-  # persons that follow user
   has_many :followed_by_relationships, foreign_key: :followed_id, class_name: 'Following'
   has_many :followed_by, through: :followed_by_relationships, source: :follower
 
+  after_create :follow_self
 
   def add_follower!(follower)
     if followed_by.include?(follower)
@@ -17,5 +16,11 @@ class User < ApplicationRecord
     end
 
     followed_by_relationships.create(follower: follower)
+  end
+
+  private
+
+  def follow_self
+    following_relationships.create(followed_id: id)
   end
 end
